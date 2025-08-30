8b8740166a751d18a55e45b0b2822829859f97ce
haiyinlin <155143624@qq.com>
2025-08-31 03:42:30 +0800

    Update x86_64-part2.sh

--- x86_64-part2.sh
+++ x86_64-part2.sh
@@ -17,6 +17,6 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # 加入万吉
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.253/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.250/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/luci2/bin/config_generate

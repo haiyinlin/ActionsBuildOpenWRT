0b9ffc7a257991e0b8ebfd0d47b8372a9fe55c6c
haiyinlin <155143624@qq.com>
2025-09-01 15:46:18 +0800

    Update x86_64-part2.sh
    del mosdns

--- x86_64-part2.sh
+++ x86_64-part2.sh
@@ -2,18 +2,6 @@
 
 
 
-# 1. 删除源码自带的旧版 mosdns 和 v2ray-geodata
-#    脚本的当前目录已经是 openwrt/，所以可以直接操作
-echo 'Removing old mosdns and v2ray-geodata...'
-find ./feeds -maxdepth 3 -type f -path '*/luci-app-mosdns/Makefile' | xargs -r rm -f
-find ./feeds -maxdepth 3 -type d -path '*/v2ray-geodata' | xargs -r rm -rf
-
-# 2. 克隆 sbwml 的新版 mosdns 和 v2ray-geodata 源码
-echo 'Cloning new mosdns and v2ray-geodata...'
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-
 # 加入万吉
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 # Modify default IP
